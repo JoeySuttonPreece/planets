ac2adbd1ec4b8b62bc168872b41ab454564011d9
Joey Sutton Preece <102568393@student.swin.adu.au>
2019-10-23 20:03:10 +1100

    adding planets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PlanetService } from './planet.service';
+import { Planet } from './planet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'planets';
+  inputName: string = '';
+  inputSize: number = 0;
+  constructor(public planetService: PlanetService) {}
+
+  createPlanet() {
+    if (this.inputName != '') {
+      this.planetService.addPlanet(new Planet(this.inputName, this.inputSize));
+      this.inputName = '';
+      this.inputSize = 0;
+    }
+  }
 }
--- src/app/planet.service.ts
+++ src/app/planet.service.ts
@@ -5,19 +5,17 @@ import { Planet } from './planet';
   providedIn: 'root'
 })
 export class PlanetService {
-  private planets: Planet[] = [
-    new Planet('earth', 1),
-    new Planet('mars', 0.7),
-    new Planet('jupiter', 23),
+  planets: Planet[] = [
+    new Planet('Earth', 1),
+    new Planet('Mars', 0.7),
+    new Planet('Jupiter', 23),
   ];
-
-  getPlanets() {
-    return this.planets;
-  }
 
   addPlanet(planet: Planet) {
     this.planets.push(planet);
   }
 
-
+  sortPlanets() {
+    this.planets.sort((a: Planet, b: Planet) => ((a.name > b.name) ? -1 : 1))
+  }
 }
